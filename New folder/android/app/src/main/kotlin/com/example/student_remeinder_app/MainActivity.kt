68ff058f925bd8585659c196cc2bcd0691220633
Shana-kay Daley <daleyshanakay32@gmail.com>
2025-09-09 09:53:29 -0500

    FEAT: new updates

--- New folder/android/app/src/main/kotlin/com/example/student_remeinder_app/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.student_remeinder_app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- New folder/android/app/src/main/kotlin/com/example/student_reminder_x/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.student_reminder_x
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
