af5cd4dd1c51010a305a9cb59c644f9e58c9c9f9
livingstonmitchell <livingstonmitchell18@gmail.com>
2025-09-25 09:28:22 -0500

    .

--- lib/src/services/attendance_service.dart
+++ lib/src/services/attendance_service.dart
@@ -551,6 +551,7 @@ class AttendanceService {
         .orderBy('dayId')
         .snapshots();
   }
+  
 
   /// Send notification to user for admin actions on their attendance
   static Future<void> _sendAdminActionNotification({
